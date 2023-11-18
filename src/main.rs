3910689c41b5cfb547c2cc919c6d47c1cd3f2dc4
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-11-18 03:54:45 +0200

    Implement gender lookup

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,57 @@
-use anyhow::{anyhow, Result};
+use anyhow::anyhow;
+use colored::Colorize;
 use std::env;
 
-fn main() -> Result<()> {
-    let filepath = env::args()
-        .nth(1)
-        .ok_or_else(|| anyhow!("File path argument not provided"))?;
+use dict_cc_lookup::query;
+
+fn main() -> anyhow::Result<()> {
+    let query: query::Query = env::args().skip(1).collect::<Vec<String>>().try_into()?;
+    let word = match query {
+        query::Query::Gender(word) => word,
+        _ => return Err(anyhow!("unsupported query")),
+    };
+
+    let dict_archive = zstd::stream::read::Decoder::new(&include_bytes!("dict.txt.zst")[..])?;
 
     let mut reader = csv::ReaderBuilder::new()
         .has_headers(false)
         .delimiter(b'\t')
         .flexible(true)
-        .from_path(filepath)?;
+        .from_reader(dict_archive);
 
-    let mut previous = String::new();
-    let mut meanings = Vec::<String>::new();
-
-    fn show(word: &str, meanings: &[String]) {
-        println!("{} = {}", word, meanings.join(", "))
-    }
-
-    for res in reader.records() {
-        let record = res?;
-        let first: Vec<_> = record.get(0).unwrap().split_whitespace().collect();
-        let gender = first.last().cloned().unwrap();
-        if !first.iter().any(|v| v.ends_with("gefühl")) || !gender.contains('{') || first.len() != 2
+    for rec in reader.records() {
+        let cols: [_; 4] = match rec?
+            .into_iter()
+            .map(String::from)
+            .collect::<Vec<_>>()
+            .try_into()
         {
+            Ok(cols) => cols,
+            Err(_) => continue,
+        };
+
+        let parts: Vec<_> = cols[0].split_whitespace().collect();
+        if parts.len() < 2 || !parts.iter().any(|&v| v == word) {
             continue;
         }
 
-        let word: String = first.iter().take(first.len() - 1).cloned().collect();
-        let gender = match gender {
-            "{n}" => "das",
-            _ => unreachable!(),
+        let (gender_text, is_plural) = match *parts.last().unwrap() {
+            "{m}" => ("der".bright_blue(), false),
+            "{f}" => ("die".bright_magenta(), false),
+            "{n}" => ("das".bright_green(), false),
+            "{pl}" => ("die".bright_yellow(), true),
+            _ => continue,
         };
 
-        if word != previous {
-            if !previous.is_empty() {
-                show(&previous, &meanings);
-            }
+        print!(
+            "{} {}{}",
+            gender_text.bold(),
+            word,
+            if is_plural { " (pl)" } else { "" }
+        );
 
-            previous = gender.to_owned() + " " + &word;
-            meanings.clear();
-        }
-
-        meanings.push(record.get(1).unwrap().into());
+        return Ok(());
     }
 
-    show(&previous, &meanings);
-
-    Ok(())
+    Err(anyhow!("not found"))
 }
---
+++ src/query.rs
@@ -0,0 +1,74 @@
+use std::ops::Deref;
+
+use anyhow::anyhow;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Language {
+    German,
+    English,
+}
+
+#[derive(Debug, Clone)]
+pub enum Query {
+    Gender(String),
+    Meaning {
+        language: Language,
+        components: Vec<String>,
+        verbose: bool,
+    },
+}
+
+impl TryFrom<Vec<String>> for Query {
+    type Error = anyhow::Error;
+
+    fn try_from(mut value: Vec<String>) -> anyhow::Result<Self> {
+        if value.is_empty() {
+            return Err(anyhow!("input is empty"));
+        }
+
+        let maybe_specifier = value.remove(0);
+
+        if value.is_empty() {
+            return Ok(Query::Meaning {
+                language: Language::German,
+                components: maybe_specifier
+                    .split_whitespace()
+                    .map(String::from)
+                    .collect(),
+                verbose: false,
+            });
+        }
+
+        let (language, verbose) = match maybe_specifier.to_lowercase().deref() {
+            "g" => {
+                let word =
+                    to_upper(value.first().unwrap()).ok_or_else(|| anyhow!("empty input"))?;
+                return Ok(Query::Gender(word));
+            }
+            "d" => (Language::German, false),
+            "dv" => (Language::German, true),
+            "e" => (Language::English, false),
+            "ev" => (Language::English, true),
+            _ => return Err(anyhow!("invalid query specifier \"{}\"", maybe_specifier)),
+        };
+
+        let components: Vec<_> = value
+            .iter()
+            .flat_map(|v| v.split_whitespace())
+            .map(String::from)
+            .collect();
+
+        Ok(Query::Meaning {
+            language,
+            components,
+            verbose,
+        })
+    }
+}
+
+fn to_upper(s: &str) -> Option<String> {
+    let mut chars = s.chars();
+    chars
+        .next()
+        .map(|c| c.to_uppercase().to_string() + chars.as_str())
+}
